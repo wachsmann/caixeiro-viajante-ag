fe3737ca3f6a3864fd04a0b8377d4b60f8a2f9fb
Leonardo Dalbosco <leodalbosco@hotmail.com>
2019-06-15 20:19:15 -0300

    Criação da matriz de adjacência

--- src/caixeiro/viajante/ag/CaixeiroViajanteAg.java
+++ src/caixeiro/viajante/ag/CaixeiroViajanteAg.java
@@ -32,14 +32,72 @@ public class CaixeiroViajanteAg {
             readFile("teste.txt");
             for (Cities cities : citiesList) {
                 ArrayList<Integer> distances = cities.getDistances();
-                System.out.println("Quantidade de cidades: "+ cities.getQuantity());
+                distances.remove(0);
                 
-                for (int i = 1; i < distances.size(); i++) {
-                    System.out.println(" Distância entre duas cidades " + distances.get(i));
+                int matriz[][] = new int[cities.getQuantity()][cities.getQuantity()];
+
+                System.out.println(distances);
+                System.out.println("\n");
+                
+                // Inicialização da matriz com "0"
+                for (int i = 0; i < cities.getQuantity(); i++) {
+                    for (int j = 0; j < cities.getQuantity(); j++) {
+                        if (i == j) {
+                            matriz[i][j] = 0;
+                        } else {
+                            matriz[i][j] = -1;
+                        }
+                    }
                 }
-            }
                 
-            
+                // Declara variaveis para poder utilizar
+                int aux = 0, aux_2 = 1;
+                
+                // Percorre as distancias de cada cidade
+                for (int i = 0; i < distances.size(); i++) {
+                    
+                    // Caso checar no final da matriz entra
+                    if (aux_2 >= cities.getQuantity()) {
+                        
+                        if (matriz[aux][0] == -1 || matriz[0][aux] == -1) {
+                            matriz[aux][0] = distances.get(i);
+                            matriz[0][aux] = distances.get(i);
+                        }
+                        
+                        
+                        // Percorre verificando todas as conexões e adiciona caso não tenha 
+                        for (int j = 0; j < cities.getQuantity(); j++) {
+                            for (int k = 0; k < cities.getQuantity(); k++) {
+                                if (matriz[j][k] == -1 || matriz[k][j] == -1) {
+                                    i++;
+                                    matriz[j][k] = distances.get(i);
+                                    matriz[k][j] = distances.get(i);
+                                }
+                            }
+                        }
+                        
+                        break;
+                    }
+                    
+                    if (matriz[aux][aux_2] == -1 || matriz[aux_2][aux] == -1) {
+                        matriz[aux][aux_2] = distances.get(i);
+                        matriz[aux_2][aux] = distances.get(i);
+                    }
+                    
+                    aux++;
+                    aux_2++;
+                }
+                
+                String saida = "";
+                for (int i = 0; i < cities.getQuantity(); i++) {
+                    for (int j = 0; j < cities.getQuantity(); j++) {
+                        saida += matriz[i][j] + "\t";
+                    }
+                    saida += "\n";
+                }
+                
+                System.out.println(saida);
+            }
         } catch (IOException ex) {
             Logger.getLogger(CaixeiroViajanteAg.class.getName()).log(Level.SEVERE, null, ex);
         }
