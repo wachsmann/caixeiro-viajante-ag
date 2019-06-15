8fb0f8d3a813ef5e9d38f3fd5e48e0daba624cbf
wachsmann <victor.wachsmann@webtouch.com.br>
2019-06-15 09:02:59 -0300

    missing files

---
+++ src/caixeiro/viajante/ag/Cities.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caixeiro.viajante.ag;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author wachsmann
+ */
+public class Cities {
+    
+    public Cities(Integer quantity){
+        if(quantity < 21 && quantity > 0){
+            this.quantity = quantity;
+            this.distances = new ArrayList<>();
+        }else{
+            throw new ExceptionInInitializerError("Quantidade inválida");
+        }
+        
+    }
+    
+    private Integer quantity = 0;
+    
+    private ArrayList<Integer> distances;
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+    public ArrayList<Integer> getDistances() {
+        return distances;
+    }
+
+    public void addDistance(Integer distance){
+            this.distances.add(distance);
+    }
+    
+}
